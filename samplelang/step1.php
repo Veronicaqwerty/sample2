9ba1502bc894a80f84d5443b35502d315e1ee763
Veronicaqwerty <125360832+Veronicaqwerty@users.noreply.github.com>
2024-01-22 16:21:08 +0800

    Add files via upload

---
+++ samplelang/step1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VERyWELL</title>
+    <style type="text/css">
+        html, body {
+            margin: 0;
+            font-family: 'Courier New', monospace;
+            background-color: #7d5a43;
+            color: #fff;
+            background-image: url('ww.png');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    <!-- content goes here-->
+</body>
+</html>
